50127cfbfc078164f61ded5138d62ae9181fe68b
GonzaloMdC <gaguirre@metallsdelcampslu.es>
2025-02-12 15:22:05 +0100

    Vuelta al origen para que funcione todo correctamente

--- wasty/obj/Debug/net9.0-windows/Views/MainView.g.i.cs
+++ wasty/obj/Debug/net9.0-windows/Views/MainView.g.i.cs
@@ -55,7 +55,7 @@ namespace wasty.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/wasty;component/views/mainview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/wasty;V1.0.0.0;component/views/mainview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\MainView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
