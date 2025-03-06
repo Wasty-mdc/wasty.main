ccc7d761f2bdb0583bc45f8be06acab17dde57c5
Henry FlantrmskyUsuario <hflantrmsky@metallsdelcampslu.es>
2025-03-06 17:42:50 +0100

    . Ajstes funcionales

--- wasty/Models/Cliente.cs
+++ wasty/Models/Cliente.cs
@@ -4,7 +4,7 @@ namespace wasty.Models
 {
     public class Cliente
     {
-        [JsonPropertyName("codCliente")]
+        [JsonPropertyName("id")]
         public int Codigo { get; set; }
         [JsonPropertyName("nombreComercial")]
         public string NombreComercial { get; set; }
@@ -14,8 +14,8 @@ namespace wasty.Models
         public string NIF { get; set; }
         [JsonPropertyName("telefono")]
         public string Telefono { get; set; }
-        [JsonPropertyName("email")]
-        public string Email { get; set; }
+        [JsonPropertyName("nima")]
+        public string Nima { get; set; }
         public string Estado { get; set; } = "Activo";
     }
 }
--- wasty/Models/Residuo.cs
+++ wasty/Models/Residuo.cs
@@ -9,15 +9,9 @@ namespace wasty.Models
 {
     public class Residuo
     {
-        [JsonPropertyName("codResiduo")]
-        public int Codigo { get; set; }
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
         [JsonPropertyName("nombre")]
-        public string Tipo { get; set; }
-        public double Peso { get; set; }
-        public string Estado { get; set; }
-        public string Ubicacion { get; set; }
-        public DateTime FechaRecoleccion { get; set; }
-        public string EmpresaGestora { get; set; }
-        public string Destino { get; set; }
+        public string Nombre { get; set; }
     }
 }
