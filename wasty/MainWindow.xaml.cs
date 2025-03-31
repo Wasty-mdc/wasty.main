8444fcbee8170c94ddac5f4ace61a7642af7ee87
dgtoni <aalcazar@metallsdelcampslu.es>
2025-03-31 16:07:19 +0200

    BdA a mitad y Ventanas arreglado

--- wasty/MainWindow.xaml.cs
+++ wasty/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Interop;
 using Microsoft.Win32;
 using wasty.ViewModels;
@@ -9,14 +10,24 @@ namespace wasty
 {
     public partial class MainWindow : Window
     {
+        private IntPtr hwnd;
+
+        private const int GWL_STYLE = -16;
+        private const int WS_CAPTION = 0x00C00000;
+
         private const int DWMWA_CAPTION_COLOR = 35;
         private const int DWMWA_TEXT_COLOR = 34;
         private const int DWMWA_USE_IMMERSIVE_DARK_MODE = 20;
 
-        private IntPtr hwnd;
+        [DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
 
         [DllImport("dwmapi.dll")]
         private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref uint attrValue, int attrSize);
+
         [DllImport("dwmapi.dll")]
         private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
 
@@ -27,13 +38,20 @@ namespace wasty
 
             Loaded += MainWindow_Loaded;
             Unloaded += MainWindow_Unloaded;
-
             SystemEvents.UserPreferenceChanged += SystemEvents_UserPreferenceChanged;
         }
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
             hwnd = new WindowInteropHelper(this).Handle;
+
+            // Eliminar barra blanca (compositor y estilos)
+            HwndSource.FromHwnd(hwnd).CompositionTarget.BackgroundColor = System.Windows.Media.Colors.Transparent;
+
+            int style = GetWindowLong(hwnd, GWL_STYLE);
+            style &= ~WS_CAPTION;
+            SetWindowLong(hwnd, GWL_STYLE, style);
+
             ApplyWindowTheme();
         }
 
@@ -62,6 +80,43 @@ namespace wasty
             DwmSetWindowAttribute(hwnd, DWMWA_TEXT_COLOR, ref textColor, sizeof(uint));
             DwmSetWindowAttribute(hwnd, DWMWA_USE_IMMERSIVE_DARK_MODE, ref useDarkMode, sizeof(int));
         }
+
+        private void Barra_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                MaximizarRestaurar_Click(sender, e);
+            }
+            else
+            {
+                DragMove();
+            }
+        }
+
+        private void Minimizar_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizarRestaurar_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+                MaxRestoreIcon.Kind = MaterialDesignThemes.Wpf.PackIconKind.WindowMaximize;
+            }
+            else
+            {
+                WindowState = WindowState.Maximized;
+                MaxRestoreIcon.Kind = MaterialDesignThemes.Wpf.PackIconKind.WindowRestore;
+            }
+        }
+
+        private void Cerrar_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
     }
 
     public static class ThemeHelper
@@ -74,19 +129,15 @@ namespace wasty
                 {
                     if (key != null)
                     {
-                        object registryValueObject = key.GetValue("AppsUseLightTheme");
-                        if (registryValueObject != null)
+                        object value = key.GetValue("AppsUseLightTheme");
+                        if (value != null)
                         {
-                            int registryValue = (int)registryValueObject;
-                            return registryValue == 0; // 0 = oscuro, 1 = claro
+                            return (int)value == 0;
                         }
                     }
                 }
             }
-            catch
-            {
-                // Si falla, asumimos tema claro
-            }
+            catch { }
 
             return false;
         }
