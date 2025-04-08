99843db1c7baf594c7f723c93dbdf92650873e81
GonzaloMdC <gaguirre@metallsdelcampslu.es>
2025-04-08 13:07:43 +0200

    Logins Ajustados

--- wasty/ViewModels/LoginViewModel.cs
+++ wasty/ViewModels/LoginViewModel.cs
@@ -22,6 +22,11 @@ class LoginViewModel : LoginModel
         _navigationService = navigationService;
         LoginCommand = new RelayCommand(async _ => await LoginAsync());
         SkipCommand = new RelayCommand(async _ => await SkipLogin());
+        SkipCommandG = new RelayCommand(async _ => await SkipLoginG());
+        SkipCommandS = new RelayCommand(async _ => await SkipLoginS());
+        SkipCommandT = new RelayCommand(async _ => await SkipLoginT());
+        SkipCommandH = new RelayCommand(async _ => await SkipLoginH());
+
         RegisterCommand = new RelayCommand(_ => _navigationService.NavigateTo<SignupView>());
         SnackbarMessageQueue = new SnackbarMessageQueue(TimeSpan.FromSeconds(3));
         _authService = authService;
@@ -29,6 +34,10 @@ class LoginViewModel : LoginModel
 
     // Comandos para iniciar sesión y registrarse
     public ICommand LoginCommand { get; }
+    public ICommand SkipCommandG { get; }
+    public ICommand SkipCommandS { get; }
+    public ICommand SkipCommandT { get; }
+    public ICommand SkipCommandH { get; }
     public ICommand RegisterCommand { get; }
     public ICommand SkipCommand { get; }
 
@@ -57,6 +66,45 @@ class LoginViewModel : LoginModel
         // Definir credenciales predeterminadas
         Email = "Pruebas123@pruebas.com";
         Contrasenia = "Pruebas123.";
+
+        // Llamar directamente a la función de Login
+        await LoginAsync();
+    }
+
+    private async Task SkipLoginG()
+    {
+        // Definir credenciales predeterminadas
+        Email = "Gonzalo@pruebas.com";
+        Contrasenia = "Gonzalo123.";
+
+        // Llamar directamente a la función de Login
+        await LoginAsync();
+    }
+
+    private async Task SkipLoginS()
+    {
+        // Definir credenciales predeterminadas
+        Email = "Santi@pruebas.com";
+        Contrasenia = "Santi123.";
+
+        // Llamar directamente a la función de Login
+        await LoginAsync();
+    }
+
+    private async Task SkipLoginT()
+    {
+        // Definir credenciales predeterminadas
+        Email = "Toni@pruebas.com";
+        Contrasenia = "Toni123.";
+
+        // Llamar directamente a la función de Login
+        await LoginAsync();
+    }
+    private async Task SkipLoginH()
+    {
+        // Definir credenciales predeterminadas
+        Email = "Henry@pruebas.com";
+        Contrasenia = "Henry123.";
 
         // Llamar directamente a la función de Login
         await LoginAsync();
