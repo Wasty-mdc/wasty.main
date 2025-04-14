244870fc0d63ba463dc0728a3038abc747537b87
GonzaloMdC <gaguirre@metallsdelcampslu.es>
2025-04-14 13:46:53 +0200

    Tratments con MockData

---
+++ wasty/Models/Treatment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wasty.Models
+{
+    public class Treatment
+    {
+        public string Codigo { get; set; }
+        public string Fecha { get; set; } 
+        public string Denominacion { get; set; }
+        public string LER { get; set; }
+        public string OrigenDestino { get; set; }
+        public string OperadorTraslados { get; set; }
+        public string Activo { get; set; }
+        public string Notificar { get; set; }
+    }
+}
