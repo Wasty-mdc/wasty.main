05de3cc6a90576ceba9ab077e393a6dbf09d5eae
GonzaloMdC <gaguirre@metallsdelcampslu.es>
2025-05-08 16:04:18 +0200

    Vista de Traslados

---
+++ wasty/Models/TrasladoModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wasty.Models
+{
+    internal class TrasladoModel
+    {
+        public string Codigo { get; set; }
+        public string FechaInicio { get; set; }
+        public string FechaFin { get; set; }
+        public string Transportista { get; set; }
+        public string OrigenDestino { get; set; }
+        public string Chofer { get; set; }
+        public string Matricula { get; set; }
+        public string StatusTrasla { get; set; } // para el botón de estado
+    }
+}
